501f990b872cc5b4874ba4d7a7195c7f7a5a939a
Ranjith Rana Rudra <srisasanka096@gmail.com>
2019-08-15 22:38:27 +0530

    changes committed by sasanka

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+import React, {useState} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {id: 1, name: 'Jiang Chen', age: Math.round(Math.random() * 30)},
+      {id: 2, name: 'Qin Wentian', age: Math.round(Math.random() * 30)},
+      {id: 3, name: 'Chen Xi', age: Math.round(Math.random() * 30)},
+    ]
+  });
+  
+  const [otherState, setOtherState] = useState('Some Other State');
+
+  const [showPersonsState, setShowPersonsState] = useState({
+    showpersons: false
+  });
+
+  const nameChangeHandler = (event, id) => {
+    const personIndex = personsState.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...personsState.persons[personIndex]};
+    person.name = event.target.value;
+
+    const persons = [...personsState.persons];
+    persons[personIndex] = person;
+
+    setPersonsState({
+      persons: persons
+    });
+  };
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    boeder: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+  };
+
+  let persons = null;
+  if (showPersonsState.showpersons) {
+    persons = 
+      <div>
+        {personsState.persons.map((person, index) => {
+          return <Person
+            click= {() => deletePersonhandler(index)}
+            name={person.name} 
+            age={person.age} 
+            key = {person.id}
+            change = {(event) => nameChangeHandler(event, person.id)}
+          />
+        })}
+      </div>
+    ;
+  }
+
+  const togglePersonsHandler = (event) => {
+    setShowPersonsState({
+      showpersons: !showPersonsState.showpersons
+    });
+  }
+
+  const deletePersonhandler = (personIndex) => {
+    const persons = [...personsState.persons];
+    persons.splice(personIndex, 1);
+    setPersonsState({
+      persons: persons
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> I'm React App !!!</h1>
+      <button
+        style = {style}
+        onClick= {togglePersonsHandler}
+      > Toggle Persons </button>
+      {persons}
     </div>
   );
 }
 
-export default App;
+export default app;
